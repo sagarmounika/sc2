72b1abc979775d5909c2cffeac921c748e22a32b
sagar mounika <sagarmouni5@gmail.com>
2021-03-02 14:47:42 +0530

    carousel and video

---
+++ src/Components/Video.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactPlayer from "react-player"
+const Video = () => {
+    return (
+        <div className="video">
+          <ReactPlayer
+        url="https://www.youtube.com/watch?v=ug50zmP9I7s"
+      />  
+        </div>
+    )
+}
+
+export default Video
